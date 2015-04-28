cc37f53460afd7dbe60ba4e00c3669bfc3ea45c5
grudov <vagrant@precise32.(none)>
2015-04-28 19:20:27 +0000

    Finished first out of the User model

---
+++ sources/sample_app/spec/models/user_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe User do
+  
+  before(:each) do
+    @attr = {nom: "Exemple Use", email: "user@exemple.com"}
+  end
+  
+  it "Devrait cree une nouvelle instance dotee des attributs valides" do
+    User.create!(@attr)
+  end
+  
+  it "Devrait exiger un nom" do
+    bad_guy = User.new(@attr.merge(nom: ""))
+    bad_guy.should_not be_valid
+  end
+  
+  it "exige une adresse email" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+  
+  it "devrait rejeter les noms trop long" do
+    long_nom = "a" * 51
+    long_nom_user = User.new(@attr.merge(nom: long_nom))
+    long_nom_user.should_not be_valid
+  end
+  
+  it "devrait accepter une adresse mail valide" do
+    adresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    adresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+  
+  it "devrait rejeter une adresse mail invalide" do
+    adresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    adresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+  
+  it "devrait rejeter un email double" do 
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+  
+  it "devrait rejeter une adresse email invalide jusqu'a la casse" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+  
+end
