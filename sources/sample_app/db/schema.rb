fcbcfc23342b6fb5eb332d26455b3027821f80f2
grudov <vagrant@precise32.(none)>
2015-04-29 08:21:50 +0000

    Modele utilisateur avec mot de passe et page profil

--- sources/sample_app/db/schema.rb
+++ sources/sample_app/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150428142429) do
+ActiveRecord::Schema.define(:version => 20150428204810) do
 
   create_table "users", :force => true do |t|
     t.string   "nom"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
---
+++ sources/sample_app/spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.nom                    "Grudov"
+  user.email                  "grudov@github.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
